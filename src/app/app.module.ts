80098f53155cd2fd23c92947e1e35193b56899bf
Angie Badillo <hunnybunny@live.com.mx>
2019-05-13 23:49:13 -0700

    SUCCESS COLOR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessComponent } from './success/success.component';
 import { WarningComponent } from './warning/warning.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { WarningComponent } from './warning/warning.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,16 +2,48 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+allowNewServer = false;
+serverCreationStatus = 'No server was created';
+serverName = '';
+serverCreated = false;
+servers = ['testserver', 'testserver 2']
+userName = '';
 
-  constructor() { }
+showSecret = false;
+log = [];
+
+onToggleDetails() {
+  this.showSecret = !this.showSecret;
+  this.log.push(new Date());
+}
+
+  constructor() { setTimeout( () => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit() {
   }
 
-}
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'server was created. Server name is ' + this.serverName;
+
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+    console.log(event);
+  }
+
+  onUpdateUserName(event: Event) {
+    this.userName = (<HTMLInputElement>event.target).value;
+    console.log(event);
+  }
+  }
+
+
